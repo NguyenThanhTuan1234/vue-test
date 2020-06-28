e94d5eaf6cad1b08d48c318ba9782a7c55f5768f
TUANKUN <Tuankun@TuanKunnoMacBook-Pro.local>
2020-06-28 22:50:10 +0900

    Ageのデータタイプを文字列から数字に変更する

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.12
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.7.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/tools v0.0.0-20200626171337-aa94e735be7f // indirect
 )
--- main.go
+++ main.go
@@ -1,34 +1,96 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
 )
 
-type Task struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Test string `json: "test"`
+type H map[string]interface{}
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "postgres"
+	DB_NAME     = "test"
+	SSL         = "disable"
+)
+
+type User struct {
+	UserID   int
+	Name     string
+	Age      int `json:",string"`
+	Location string
 }
-type TaskCollection struct {
-	Tasks []Task `json:"items"`
+type Users struct {
+	Users []User `json:"users"`
 }
 
-func GetTask() echo.HandlerFunc {
+// var DB *sql.DB
+var err error
+
+func DBinit() *sql.DB {
+	var err error
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s",
+		DB_USER, DB_PASSWORD, DB_NAME, SSL)
+	DB, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Success")
+	return DB
+}
+func GetUser(db *sql.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var task Task
-		c.Bind(&task)
-		fmt.Println(task.Name)
-		fmt.Println(task.Test)
+		var user User
+
+		c.Bind(&user)
+		fmt.Println(user.Name)
+		fmt.Println(user.Age)
+		fmt.Println(user.Location)
+		// age, err := strconv.Atoi(user.Age)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// id, err := PutUser(db, user.Name, age, user.Location)
+		// if err == nil {
+		// 	return c.JSON(http.StatusCreated, H{
+		// 		"created": id,
+		// 	})
+		// 	// Handle any errors
+		// } else {
+		// 	return err
+		// }
 		return nil
 	}
 }
 
+func PutUser(db *sql.DB, name string, age int, location string) (int64, error) {
+	sql := "INSERT INTO users(name, age, location) VALUES($1, $2, $3)"
+	stmt, err := db.Prepare(sql)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(name, age, location)
+	if err != nil {
+		panic(err)
+	}
+	return result.LastInsertId()
+}
+
 func main() {
 	e := echo.New()
-
-	e.File("/", "index.html")
-	e.PUT("/tasks", GetTask())
+	DB := DBinit()
+	e.File("/", "public/index.html")
+	e.Static("/*", "public")
+	e.Static("/css/*", "public/css")
+	e.Static("/fonts/*", "public/fonts")
+	e.Static("/js/*", "public/js")
+	e.Static("/images/*", "public/images")
+	e.Static("/sass/*", "public/sass")
+	e.File("/users", "index.html")
+	e.POST("/users", GetUser(DB))
 	e.Logger.Fatal(e.Start(":1234"))
 }
